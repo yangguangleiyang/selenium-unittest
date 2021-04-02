0b01bd8f0b26402c448342150cc3d148be1597e1
yangguangleiyang <15712895616@163.com>
2021-04-02 18:08:23 +0800

    搭建selenium环境

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
-print("hello world1")
+# print("hello world1")
+
+from selenium import webdriver
+
+driver_path=r"C:\chromedriver_win32\chromedriver.exe"
+
+driver=webdriver.Chrome(executable_path=driver_path)
+
+driver.get("http://baidu.com")
