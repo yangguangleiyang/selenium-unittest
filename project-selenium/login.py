349b9e82aa7b15958c432682527daf4f00c90fb1
yangguangleiyang <15712895616@163.com>
2021-04-07 19:57:45 +0800

    2021.4.7 Selenium项目实战

---
+++ project-selenium/login.py
@@ -0,0 +1,120 @@
+import time
+
+from selenium import webdriver
+
+driver_path=r"C:\chromedriver_win32\chromedriver.exe"
+
+driver=webdriver.Chrome(executable_path=driver_path)
+
+driver.get("http://127.0.0.1:8001/login/")
+driver.maximize_window()
+driver.implicitly_wait(5)
+
+total = 0
+pass_case = 0
+############登录正向用例##############
+print("用例1：登录正向用例")
+username,password,randcode ="admin","123456","123"
+driver.find_element_by_id("LAY-user-login-username").send_keys(username)
+driver.find_element_by_id("LAY-user-login-password").send_keys(password)
+driver.find_element_by_id("LAY-user-login-vercode").send_keys(randcode)
+driver.find_element_by_id("loginButton").click()
+
+total+=1
+#增加断言方式一：通过标题来断言
+"""
+time.sleep(3)
+page_title = driver.title
+if page_title == "自动化测试web项目":
+    print("测试通过")
+else:
+    print("测试失败")
+"""
+
+#增加断言方式二：通过页面元素来断言
+
+assertObject = driver.find_element_by_css_selector("body > div > div.layui-header > div")
+if assertObject and assertObject.text == "接口自动化测试":
+    pass_case+=1
+    print("测试通过")
+else:
+    print("测试失败")
+
+driver.close()
+
+############登录反向用例_用户名错误##############
+print("用例2：用户名错误")
+driver=webdriver.Chrome(executable_path=driver_path)
+
+driver.get("http://127.0.0.1:8001/login/")
+driver.maximize_window()
+driver.implicitly_wait(5)
+
+
+username,password,randcode ="admin","12345","123"
+driver.find_element_by_id("LAY-user-login-username").send_keys(username)
+driver.find_element_by_id("LAY-user-login-password").send_keys(password)
+driver.find_element_by_id("LAY-user-login-vercode").send_keys(randcode)
+driver.find_element_by_id("loginButton").click()
+total+=1
+
+#增加断言
+rspmsg = driver.find_element_by_css_selector("#layui-layer2 > div.layui-layer-content.layui-layer-padding")
+if rspmsg and rspmsg.text == "用户名或密码错误":
+    pass_case += 1
+    print("测试通过")
+else:
+    print("测试失败")
+
+driver.close()
+
+############登录反向用例_密码错误##############
+print("用例3：密码错误")
+driver = webdriver.Chrome(executable_path=driver_path)
+
+driver.get("http://127.0.0.1:8001/login/")
+driver.maximize_window()
+driver.implicitly_wait(5)
+
+username, password, randcode = "admin1", "123456", "123"
+driver.find_element_by_id("LAY-user-login-username").send_keys(username)
+driver.find_element_by_id("LAY-user-login-password").send_keys(password)
+driver.find_element_by_id("LAY-user-login-vercode").send_keys(randcode)
+driver.find_element_by_id("loginButton").click()
+total+=1
+
+# 增加断言
+rspmsg = driver.find_element_by_css_selector("#layui-layer2 > div.layui-layer-content.layui-layer-padding")
+if rspmsg and rspmsg.text == "用户名或密码错误":
+    pass_case += 1
+    print("测试通过")
+else:
+    print("测试失败")
+
+driver.close()
+
+############登录反向用例_用户名为空##############
+print("用例4：用户名为空")
+driver = webdriver.Chrome(executable_path=driver_path)
+
+driver.get("http://127.0.0.1:8001/login/")
+driver.maximize_window()
+driver.implicitly_wait(5)
+
+username, password, randcode = "admin", "123456", "123"
+# driver.find_element_by_id("LAY-user-login-username").send_keys(username)
+driver.find_element_by_id("LAY-user-login-password").send_keys(password)
+driver.find_element_by_id("LAY-user-login-vercode").send_keys(randcode)
+driver.find_element_by_id("loginButton").click()
+total+=1
+
+# 增加断言
+rspmsg = driver.find_element_by_id("layui-layer1")
+if rspmsg and rspmsg.text == "请输入用户名和密码":
+    pass_case += 1
+    print("测试通过")
+else:
+    print("测试失败")
+
+driver.close()
+print("执行用例数为：%d,执行通过：%d,执行失败：%d"%(total,pass_case,total-pass_case))
---
+++ study-selenium/jinjieyingyong.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver_path=r"C:\chromedriver_win32\chromedriver.exe"
+
+driver=webdriver.Chrome(executable_path=driver_path)
+
+#滑动拖拽  携程网注册页面
+# driver.get("https://passport.ctrip.com/user/reg/home")
+# driver.maximize_window()
+#
+# driver.find_element_by_css_selector("#agr_pop > div.pop_footer > a.reg_btn.reg_agree").click()
+# time.sleep(3)
+#
+# source = driver.find_element_by_css_selector("#slideCode > div.cpt-drop-box > div.cpt-drop-btn > div > i.cpt-logo.cpt-img-double-right")
+# box = driver.find_element_by_css_selector("#slideCode > div.cpt-drop-box > div.cpt-bg-bar")
+#
+# ActionChains(driver).drag_and_drop_by_offset(source,box.size['width'],0).perform()
+
+
+#多窗口切换
+
+driver.get("http://www.baidu.com")
+driver.maximize_window()
+driver.implicitly_wait(5)
+
+print(driver.current_window_handle)      #查看当前窗口句柄
+driver.find_element_by_link_text("新闻").click()
+
+print(driver.current_window_handle)     #继续查看当前窗口句柄
+
+handles = driver.window_handles       #查看一共开了多少窗口
+driver.switch_to.window(handles[1])
+driver.find_element_by_link_text("军事").click()
+
