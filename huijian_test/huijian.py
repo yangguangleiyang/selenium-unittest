186e1bb97489d73b7f47f16ab2c575ee483e9265
yangguangleiyang <15712895616@163.com>
2021-04-15 18:56:56 +0800

    2021-4-15

--- huijian_test/huijian.py
+++ huijian_test/huijian.py
@@ -1,13 +1,19 @@
 from selenium import  webdriver
 import time
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 
 driver_path = r"C:\chromedriver_win32\chromedriver.exe"
 driver=webdriver.Chrome(executable_path=driver_path)
 
 driver.get("https://ad-test.kuaidaoapp.com/advertise/#/?_k=9msnat")
 driver.maximize_window()
-driver.implicitly_wait(5)
+driver.implicitly_wait(10)
+time_sleep = 2
+
 
 driver.find_element_by_id("username").send_keys("yxybyzyang")
 driver.find_element_by_id("password").send_keys("123456")
@@ -30,23 +36,74 @@ else:
 
 
 #####################上传计划首字母表##########
-driver.find_element_by_css_selector("#app > div > div > div.nav-wrap.ant-layout-sider.ant-layout-sider-has-trigger > div.ant-layout-sider-children > ul > li:nth-child(8) > div > span > span").click()
-driver.find_element_by_xpath('//*[@id="7$Menu"]/li[1]/span').click()
-driver.find_element_by_css_selector("#app > div > div > div.ant-layout > div.ant-layout-content > div > div > div > div.ant-tabs-bar > div > div > div > div > div:nth-child(4)").click()
-
-#上传文件
-driver.find_element_by_css_selector("#app > div > div > div.ant-layout > div.ant-layout-content > div > div > div > div.ant-tabs-content.ant-tabs-content-animated > div.ant-tabs-tabpane.ant-tabs-tabpane-active > div > div._1x9fBCtMtv3XPSME9k73dx > span > div > span > input[type=file]").send_keys(r'C:\Users\admin\Desktop\jihuashouzimubiao.xlsx')
-driver.find_element_by_css_selector("body > div:nth-child(8) > div > div.ant-modal-wrap > div > div.ant-modal-content > div > div > div.ant-confirm-btns > button.ant-btn.ant-btn-primary.ant-btn-lg").click()
-#断言
-
-
-
+# driver.find_element_by_css_selector("#app > div > div > div.nav-wrap.ant-layout-sider.ant-layout-sider-has-trigger > div.ant-layout-sider-children > ul > li:nth-child(8) > div > span > span").click()
+# driver.find_element_by_xpath('//*[@id="7$Menu"]/li[1]/span').click()
+# driver.find_element_by_css_selector("#app > div > div > div.ant-layout > div.ant-layout-content > div > div > div > div.ant-tabs-bar > div > div > div > div > div:nth-child(4)").click()
+#
+# #上传文件
+# test_project = "测试蛋糕"
+# driver.find_element_by_css_selector("#app > div > div > div.ant-layout > div.ant-layout-content > div > div > div > div.ant-tabs-content.ant-tabs-content-animated > div.ant-tabs-tabpane.ant-tabs-tabpane-active > div > div._1x9fBCtMtv3XPSME9k73dx > span > div > span > input[type=file]").send_keys(r'C:\Users\admin\Desktop\jihuashouzimubiao.xlsx')
+# driver.find_element_by_css_selector("body > div:nth-child(8) > div > div.ant-modal-wrap > div > div.ant-modal-content > div > div > div.ant-confirm-btns > button:nth-child(1)").click()
+#
+# #再次点击计划首字母表页面，切换回来
+# time.sleep(5)
+# driver.refresh()
+# driver.find_element_by_css_selector("#app > div > div > div.ant-layout > div.ant-layout-content > div > div > div > div.ant-tabs-bar > div > div > div > div > div:nth-child(4)").click()
+# time.sleep(2)
+#
+#
+# #断言
+# all_text = driver.find_element_by_css_selector("#app > div > div > div.ant-layout > div.ant-layout-content > div > div > div > div.ant-tabs-content.ant-tabs-content-animated > div.ant-tabs-tabpane.ant-tabs-tabpane-active > div > div.ant-table-wrapper > div > div > div > div > div > table > tbody").text
+#
+# if test_project in all_text:
+#     print("计划首字母表上传成功")
+# else:
+#     print("计划首字母表上传失败")
 
 #########################新建站点############
 # driver.find_element_by_xpath('//*[@id="app"]/div/div/div[1]/div[1]/ul/li[1]/div/span/span').click()
 # time.sleep(2)
 # driver.find_element_by_xpath('//*[@id="0$Menu"]/li[1]/span').click()
-#
+# #点击新增站点按钮
 # driver.find_element_by_css_selector("#app > div > div > div.ant-layout > div.ant-layout-content > div > div > div > div.siteInfo-table > div.siteInfo-addSite > div > button:nth-child(1)").click()
-# all_handles = driver.window_handles
-# print(all_handles)
+#
+# #弹出框
+# driver.find_element_by_css_selector(".ant-form > .ant-row .ant-select-selection__placeholder").click()
+# driver.find_element_by_css_selector(".ant-select-dropdown-menu-item-active").click()
+
+
+##########新建落地页###########
+project = "CSSJ"
+
+driver.find_element_by_xpath('//*[@id="app"]/div/div/div[1]/div[1]/ul/li[2]/div/span/span').click()
+driver.find_element_by_xpath('//*[@id="1$Menu"]/li[1]/span').click()
+#点击上传落地页
+driver.find_element_by_css_selector("#app > div > div > div.ant-layout > div.ant-layout-content > div > div > div > div > div:nth-child(2) > div:nth-child(1) > button").click()
+time.sleep(time_sleep)
+
+driver.find_element_by_css_selector(".ant-col-xs-18 #pageName").send_keys("落地页1")
+driver.find_element_by_css_selector("body > div:nth-child(7) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-body > form > div:nth-child(2) > div > div > div.ant-form-item-label.ant-col-xs-5.ant-col-sm-5").click()
+time.sleep(time_sleep)
+
+driver.find_element_by_css_selector(".ant-row-flex:nth-child(2) > .ant-col-24 .ant-select-selection__placeholder").click()
+driver.find_element_by_id("domainId").send_keys("ce6.test.icloudidc.net")
+driver.find_element_by_id("domainId").send_keys(Keys.ENTER)
+driver.find_element_by_css_selector("body > div:nth-child(7) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-body > form > div:nth-child(2) > div > div > div.ant-form-item-label.ant-col-xs-5.ant-col-sm-5").click()
+time.sleep(time_sleep)
+#输入项目缩写
+driver.find_element_by_css_selector("body > div:nth-child(7) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-body > form > div:nth-child(5) > div > div > div.ant-form-item-control-wrapper.ant-col-xs-18.ant-col-sm-18 > div > div > div > div > div.ant-select-selection__placeholder").click()
+driver.find_element_by_css_selector(".ant-select-open #projectCode").send_keys(project)
+driver.find_element_by_css_selector(".ant-select-open #projectCode").send_keys(Keys.ENTER)
+time.sleep(time_sleep)
+
+#输入页面编码
+driver.find_element_by_css_selector(".ant-col-xs-18 #pageCode").click()
+driver.find_element_by_css_selector(".ant-col-xs-18 #pageCode").send_keys("123")
+time.sleep(time_sleep)
+
+#添加压缩包
+driver.find_element_by_css_selector("body > div:nth-child(7) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-body > form > div:nth-child(7) > div > div > div.ant-form-item-control-wrapper.ant-col-xs-18.ant-col-sm-18 > div > div > span > div.ant-upload.ant-upload-drag > span > input[type=file]").send_keys(r"C:\Users\admin\Desktop\py-j31-001-18094.zip")
+time.sleep(time_sleep)
+
+#点击确定按钮
+driver.find_element_by_css_selector("body > div:nth-child(7) > div > div.ant-modal-wrap > div > div.ant-modal-content > div.ant-modal-footer > button.ant-btn.ant-btn-primary.ant-btn-lg").click()
--- project-selenium/request_manage.py
+++ project-selenium/request_manage.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from selenium import webdriver
@@ -5,16 +6,85 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-driver_path=r"C:\chromedriver_win32\chromedriver.exe"
+time_sleep = 2
 
+
+#打开左侧菜单栏函数
+def gotopage(pagename):
+    driver.find_element_by_css_selector("body > div > div.layui-side.layui-bg-cyan > div > ul > li > a").click()
+    time.sleep(time_sleep)
+    driver.find_element_by_link_text(pagename).click()
+    driver.switch_to.frame(driver.find_element_by_id("mainframe"))   #切换iframe
+
+
+#提交表单
+def doFormInput(params):
+    driver.find_element_by_css_selector("#addForm > div:nth-child(2) > div.layui-input-inline > div > div > input").click()
+    # driver.find_element_by_css_selector("#addForm > div:nth-child(2) > div.layui-input-inline > div > dl > dd:nth-child(2)").click()
+    elems = driver.find_elements_by_css_selector("#addForm > div:nth-child(2) > div.layui-input-inline > div > dl > dd")
+    for ele in elems:
+        if ele.text == params["dept"]:
+            ele.click()
+            break
+    time.sleep(time_sleep)
+
+    #选择申请日期
+    driver.find_element_by_id("order_date").send_keys(params["date"])
+    driver.find_element_by_css_selector("#addForm > div:nth-child(3)").click()
+    time.sleep(time_sleep)
+
+    #选择需求名称
+    driver.find_element_by_id("order_name").send_keys(params["name"])
+    time.sleep(time_sleep)
+
+    #选择关联系统
+    driver.find_element_by_id("order_sys").send_keys(params["refers"])
+    time.sleep(time_sleep)
+
+    #选择需求类型
+    driver.find_element_by_css_selector("#addForm > div:nth-child(6) > div > div:nth-child(6) > i").click()
+    time.sleep(time_sleep)
+
+    #选择需求描述
+    driver.find_element_by_id("order_desc").send_keys(params["desc"])
+    time.sleep(time_sleep)
+
+    #提交
+    driver.find_element_by_css_selector("#submitBtn").click()
+
+
+
+def assertSucces(abc):
+    global total,pass_case
+    total += 1
+    rspmsg = ""
+    try:
+        element = WebDriverWait(driver, 10).until(
+            expected_conditions.presence_of_element_located((By.ID, 'layui-layer2'))
+        )
+
+        rspmsg = element.text
+
+    except Exception as e:
+        print(e)
+
+    if rspmsg == abc:
+        print("测试通过")
+        pass_case += 1
+    else:
+        print("测试失败")
+
+
+
+
+driver_path=r"C:\chromedriver_win32\chromedriver.exe"
 driver=webdriver.Chrome(executable_path=driver_path)
 
 driver.get("http://127.0.0.1:8001/login/")
 driver.maximize_window()
-driver.implicitly_wait(5)
+driver.implicitly_wait(5)    #隐式等待 就是如果这个元素一开始没有出现，会等待这个元素5秒，如果超过5秒还没出现，会报超时错误
 
-total = 0
-pass_case = 0
+
 ############登录##############
 
 username,password,randcode ="admin","123456","123"
@@ -23,61 +93,59 @@ driver.find_element_by_id("LAY-user-login-password").send_keys(password)
 driver.find_element_by_id("LAY-user-login-vercode").send_keys(randcode)
 driver.find_element_by_id("loginButton").click()
 
-#点击左侧菜单栏
-driver.find_element_by_css_selector("body > div > div.layui-side.layui-bg-cyan > div > ul > li > a").click()
-time.sleep(2)
-driver.find_element_by_link_text("需求申请").click()
 
-#选择需求部门
-driver.switch_to.frame(driver.find_element_by_id("mainframe"))
-driver.find_element_by_css_selector("#addForm > div:nth-child(2) > div.layui-input-inline > div > div > input").click()
-driver.find_element_by_css_selector("#addForm > div:nth-child(2) > div.layui-input-inline > div > dl > dd:nth-child(2)").click()
-time.sleep(2)
+total = 0
+pass_case = 0
+############################################
+print("用例1，正例",end="...")
+case_params = {
+    "dept":"人力部门",
+    "date":"2021-10-19",
+    "name":"测试需求名称",
+    "refers":"关联的应用系统",
+    "desc":"需求描述内容",
+    "type":"需求变更"
+}
 
-#选择申请日期
-driver.find_element_by_id("order_date").send_keys("2021-09-07")
-driver.find_element_by_css_selector("#addForm > div:nth-child(3)").click()
-time.sleep(2)
+#打开菜单
+gotopage("需求申请")
 
-#选择需求名称
-driver.find_element_by_id("order_name").send_keys("定制计划")
-time.sleep(2)
+#录入表单
+doFormInput(case_params)
 
-#选择关联系统
-driver.find_element_by_id("order_sys").send_keys("管理系统")
-time.sleep(2)
-
-#选择需求类型
-driver.find_element_by_css_selector("#addForm > div:nth-child(6) > div > div:nth-child(6) > i").click()
-time.sleep(2)
-
-#选择需求描述
-driver.find_element_by_id("order_desc").send_keys("黄花城一日游")
-time.sleep(2)
-
-#提交
-driver.find_element_by_css_selector("#submitBtn").click()
 
 #断言   等待页面元素出现才去获取，只等待10秒
-rspmsg = ""
-try:
-    element = WebDriverWait(driver,10).until(
-        expected_conditions.presence_of_element_located((By.ID,'layui-layer2'))
-    )
+assertSucces("需求登记成功.")
 
-    rspmsg = element.text
-
-except Exception as e:
-    print(e)
-
-if rspmsg == "需求登录成功":
-    print("测试通过")
-else:
-    print("测试通过")
+driver.refresh()
 
 
+############################################
+print("用例2，正例",end="...")
+case_params = {
+    "dept":"人力部门",
+    "date":datetime.date.today().strftime("%Y-%M-%D"),
+    "name":"测试需求名称",
+    "refers":"关联的应用系统",
+    "desc":"需求描述内容",
+    "type":"需求变更"
+}
+
+#打开菜单
+gotopage("需求申请")
+
+#录入表单
+doFormInput(case_params)
+
+#断言   等待页面元素出现才去获取，只等待10秒
+assertSucces("需求登记成功.")
+
+driver.quit()
 
 
+
+
+print("共执行用例：",total,"执行成功：",pass_case,"执行失败：",total-pass_case)
 
 
 
